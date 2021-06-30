c201f0d6d4706c14b0f1d128fa217b7589f182bd
sahbi96 <sahbinour@gmail.com>
2021-06-30 20:49:59 +0100

    first commit

---
+++ client/build/precache-manifest.321c371e98bade7930d26ad1ddb1b9b8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a520568e620a9bcd6931571029d826e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c0f010c9339f3c563e1",
+    "url": "/static/css/main.b27d3f4f.chunk.css"
+  },
+  {
+    "revision": "23dabc9b5106bf492c00",
+    "url": "/static/js/2.578839b3.chunk.js"
+  },
+  {
+    "revision": "c06d497d6716e84111c2fd9253bcfc02",
+    "url": "/static/js/2.578839b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c0f010c9339f3c563e1",
+    "url": "/static/js/main.7745b28e.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "8a30c0a31984acfe17e28c8499af1ad4",
+    "url": "/static/media/cool-background.8a30c0a3.png"
+  },
+  {
+    "revision": "e58ae9a23d5b156029b39601223693f8",
+    "url": "/static/media/logo.e58ae9a2.png"
+  },
+  {
+    "revision": "ac8ceeb8aec8af9ee59509e1ca9bda32",
+    "url": "/static/media/text.ac8ceeb8.PNG"
+  }
+]);
